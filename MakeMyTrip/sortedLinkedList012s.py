3eeea6fad682adb10aea48614dab11c6e6230bf0
absognety <vikasch.1994@gmail.com>
2019-12-08 18:21:32 +0530

    rearrange 0s,1s and 2s in linked list

---
+++ MakeMyTrip/sortedLinkedList012s.py
@@ -0,0 +1,82 @@
+{
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+# prints the elements of linked list starting with head
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data,end=" ")
+        curr_node=curr_node.next
+    print(' ')
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        printList(segregate(a.head))
+}
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+	Your task is to segregate the list of 
+	0s,1s and 2s.
+	
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+	Contributed By: Nagendra Jha
+'''
+def segregate(head):
+    #code here
+    global a
+    a = LinkedList()
+    curr_node = head
+    mylist = []
+    while curr_node != None:
+        mylist.append(curr_node.data)
+        curr_node = curr_node.next
+    mylist = sorted(mylist)
+    for i in mylist:
+        a.append(i)
+    return a.head
