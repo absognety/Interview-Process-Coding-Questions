c98406b70526b5edea72eb4f2da52622f3a36a4c
absognety <vikasch.1994@gmail.com>
2020-05-07 09:06:48 +0530

    Coding question #1

---
+++ LeadSquared/maximumSumLikeTimeCoefficients.py
@@ -0,0 +1,15 @@
+"""
+Given an array of N integers (negative integers allowed) which say likings of
+N foods by a customer
+
+Time taken to prepare a food arr[i] is i
+
+like-time co-efficient of food is defined as i*arr[i]. Sum of like-time
+co-efficients can be obtained by doing the add up if i*arr[i]
+
+Find the maximum sum of like-time co-efficients that can be achieved from
+a given array.
+
+Hint: Consider all combinations of likings from a given array.
+
+"""
