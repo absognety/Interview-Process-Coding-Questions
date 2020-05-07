ba49b2c92d380483f1b8023e97d24460022907c2
absognety <vikasch.1994@gmail.com>
2020-05-07 13:27:08 +0530

    fix question

--- LeadSquared/maximumSumLikeTimeCoefficients.py
+++ LeadSquared/maximumSumLikeTimeCoefficients.py
@@ -5,7 +5,7 @@ N foods by a customer
 Time taken to prepare a food arr[i] is i
 
 like-time co-efficient of food is defined as i*arr[i]. Sum of like-time
-co-efficients can be obtained by doing the add up if i*arr[i]
+co-efficients can be obtained by doing the add up of i*arr[i]
 
 Find the maximum sum of like-time co-efficients that can be achieved from
 a given array.
