7891be3d6e03315090335b1672d2867e7ee2474c
absognety <vikasch.1994@gmail.com>
2020-05-10 11:08:38 +0530

    solution added

--- LeadSquared/maximumSumLikeTimeCoefficients.py
+++ LeadSquared/maximumSumLikeTimeCoefficients.py
@@ -14,4 +14,24 @@ a given array.
 
 Hint: Consider all combinations of likings from a given array.
 
+Here is the idea:
+
 """
+
+import itertools
+
+def maxSumLikeTimeCoeff(arr,N):
+    all_sums = []
+    for r in range(1,N+1):
+        combinations = set(itertools.combinations(arr,r))
+        for x in combinations:
+            sums = 0
+            for i in range(len(x)):
+                sums += (i+1) * x[i]
+            all_sums.append(sums)
+    return max(all_sums)
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = list(map(int,input().strip().split()))
+    print (maxSumLikeTimeCoeff(arr,N))
