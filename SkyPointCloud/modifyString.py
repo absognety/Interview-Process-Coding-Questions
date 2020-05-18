113e6445125fd00538cf8304cc8dcb247dde6d3f
absognety <vikasch.1994@gmail.com>
2020-05-18 14:18:50 +0530

    Added problem statement

---
+++ SkyPointCloud/modifyString.py
@@ -0,0 +1,13 @@
+"""
+Given a string, sort/modify the characters of string according to below rules:
+    
+    1. characters having prime ascii values should come before characters
+    having composite ascii values.
+    
+    2. if two characters have same prime ascii value, then the character with
+    less value should come first.
+    
+    3. if two characters have same composite ascii value, then the character
+    with greater value should come first
+    
+"""
