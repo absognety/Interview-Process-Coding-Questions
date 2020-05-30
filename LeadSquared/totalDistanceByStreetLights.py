415eefe74a027c622f4335ef5fc08269195b08ea
absognety <vikasch.1994@gmail.com>
2020-05-30 19:35:37 +0530

    total distance covered by street lights given start and end distances (possible overlapping)

--- LeadSquared/totalDistanceByStreetLights.py
+++ LeadSquared/totalDistanceByStreetLights.py
@@ -22,3 +22,25 @@ Ex:2
     total distance = (5 + 4 - 2) = 7
     
 """
+
+def total_distance(intervals):
+    if not intervals:
+        return 0
+    if len(intervals) == 1:
+        return abs(intervals[0][0] - intervals[0][1])
+    result = 0
+    common = 0
+    intervals = sorted(intervals,key=lambda x: x[0])
+    for i in range(len(intervals)):
+        result += abs(intervals[i][0] - intervals[i][1])
+    for i in range(len(intervals)-1):
+        if intervals[i][1] > intervals[i+1][0]:
+            common += abs(intervals[i][1] - intervals[i+1][0])
+    result = abs(result - common)
+    return result
+
+if __name__ == '__main__':
+    arr1 = [(5,10)]
+    print (total_distance(arr1))
+    arr2 = [(5,10),(8,12)]
+    print (total_distance(arr2))
