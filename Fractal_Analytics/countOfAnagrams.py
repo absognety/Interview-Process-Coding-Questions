a64f0475f6b00b9e165965a80af803c540338524
absognety <vikasch.1994@gmail.com>
2021-03-29 09:52:10 +0530

    count of occurrences of anagrams of given string in text

--- Fractal_Analytics/countOfAnagrams.py
+++ Fractal_Analytics/countOfAnagrams.py
@@ -3,3 +3,18 @@ Given a text and a word, find the count of occurrences of anagrams
 of word in given text
 
 """
+
+def countAnagrams(text,word):
+    count=0
+    b = text
+    a = word
+    for i in range(len(b)-len(a)+1):
+        if(sorted(a)==sorted(b[i:i+len(a)])):
+            count=count+1
+    return count 
+
+
+if __name__ == '__main__':
+    text = str(input())
+    word = str(input())
+    print (countAnagrams(text, word))
