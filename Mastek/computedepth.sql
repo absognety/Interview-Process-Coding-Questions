1b85fc8d272b53acb6f6eba73b39dcbe4f816e35
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2024-11-25 13:28:22 +0530

    Update computedepth.sql

--- Mastek/computedepth.sql
+++ Mastek/computedepth.sql
@@ -1,1 +1,14 @@
+-- MySQL solution
 
+WITH RECURSIVE cte AS (
+   SELECT 1 AS lvl, ManagerID, EmployeeID
+   FROM employees
+   WHERE ManagerID IS NULL
+   UNION ALL
+   SELECT lvl + 1, employees.ManagerID, employees.EmployeeID
+   FROM employees
+   JOIN cte
+     ON employees.ManagerID = cte.EmployeeID
+)
+SELECT *
+FROM cte;
